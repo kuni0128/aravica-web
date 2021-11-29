054bacbeb94256be5a4e9481d24182ce48a64fa6
kuniaki <k.ishikawa.0128@gmail.com>
2021-11-30 05:59:51 +0900

    feat: add serializer

--- app/controllers/api/v1/common/origin_countries_controller.rb
+++ app/controllers/api/v1/common/origin_countries_controller.rb
@@ -3,7 +3,7 @@
 module Api::V1::Common
   class OriginCountriesController < ApplicationController
     def index
-      render json: OriginCountry.all
+      render json: OriginCountry.all, each_serializer: ::Api::V1::Common::OriginCountries::IndexSerializer
     end
   end
 end
