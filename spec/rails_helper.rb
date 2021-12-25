ff3254ef9bfee29932ee4d47030932e32a837104
kuniaki <k.ishikawa.0128@gmail.com>
2021-12-25 21:34:55 +0900

    fix: add factory_bot support file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,8 +64,6 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  config.include FactoryBot::Syntax::Methods
-
   config.before(:suite) do
     Rake::Task['db:seed'].invoke
   end
