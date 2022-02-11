89ad2aeae07ed25984926f0f005f35234dc7d6d8
kuniaki <k.ishikawa.0128@gmail.com>
2022-02-12 07:24:05 +0900

    feat: add router link

---
+++ frontend/src/pages/user/users/me/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <p>{{ pageTitle }}</p>
+    <RouterLink to="/users/me/roasted_beans">roasted beans list</RouterLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+const pageTitle = "My page works!"
+</script>
---
+++ frontend/src/pages/user/users/me/roasted_beans/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div><p>My roasted beans list works!</p></div>
+</template>
+
+<script setup lang="ts">
+</script>
