9c01fa942f73d9d9f13ae0a63cf108b358fc1c4f
kuniaki <k.ishikawa.0128@gmail.com>
2022-02-12 07:34:23 +0900

    chore: run prettier auto correct

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,16 @@
 import { createApp } from "vue"
-import {createRouter, createWebHistory, RouteRecordRaw} from "vue-router";
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router"
 import ElementPlus from "element-plus"
 import "element-plus/dist/index.css"
 import App from "./App.vue"
 
-const routes: RouteRecordRaw[] = [{ path: "/users/me", component: import("./pages/user/users/me/index.vue") }, { path: "/users/me/roasted_beans", component: import("./pages/user/users/me/roasted_beans/index.vue") }]
+const routes: RouteRecordRaw[] = [
+  { path: "/users/me", component: import("./pages/user/users/me/index.vue") },
+  {
+    path: "/users/me/roasted_beans",
+    component: import("./pages/user/users/me/roasted_beans/index.vue"),
+  },
+]
 const router = createRouter({
   history: createWebHistory(),
   routes,
