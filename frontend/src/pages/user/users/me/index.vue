6c3974a953cad153dbccf37152726cde98e75e53
kuniaki <k.ishikawa.0128@gmail.com>
2022-03-14 07:45:12 +0900

    fix: add root element to fix runtime error

--- frontend/src/pages/user/users/me/index.vue
+++ frontend/src/pages/user/users/me/index.vue
@@ -1,20 +1,22 @@
 <template>
-  <el-card>
-    <div>
-      <div>K.Iさんのコーヒー生産国ランキング</div>
-      <p>No1. ケニア 10ポイント</p>
-      <p>No2. エチオピア 7ポイント</p>
-      <p>No3. コスタリカ 5ポイント</p>
-    </div>
-  </el-card>
+  <div>
+    <el-card>
+      <div>
+        <div>K.Iさんのコーヒー生産国ランキング</div>
+        <p>No1. ケニア 10ポイント</p>
+        <p>No2. エチオピア 7ポイント</p>
+        <p>No3. コスタリカ 5ポイント</p>
+      </div>
+    </el-card>
 
-  <h2>最近購入した焙煎豆</h2>
-  <roasted-bean-list />
-  <router-link to="/users/me/roasted_beans">すべて見る</router-link>
+    <h2>最近購入した焙煎豆</h2>
+    <roasted-bean-list />
+    <router-link to="/users/me/roasted_beans">すべて見る</router-link>
 
-  <h2>最近飲んだコーヒー</h2>
-  <brewing-coffee-list />
-  <router-link to="/users/me/brewing_coffees">すべて見る</router-link>
+    <h2>最近飲んだコーヒー</h2>
+    <brewing-coffee-list />
+    <router-link to="/users/me/brewing_coffees">すべて見る</router-link>
+  </div>
 </template>
 
 <script setup lang="ts">
