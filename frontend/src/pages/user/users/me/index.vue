66d9cc4636aa38bb4191eec02372d887f3d70444
kuniaki <k.ishikawa.0128@gmail.com>
2022-03-14 07:36:18 +0900

    feat: add ranking card

--- frontend/src/pages/user/users/me/index.vue
+++ frontend/src/pages/user/users/me/index.vue
@@ -1,4 +1,13 @@
 <template>
+  <el-card>
+    <div>
+      <div>K.Iさんのコーヒー生産国ランキング</div>
+      <p>No1. ケニア 10ポイント</p>
+      <p>No2. エチオピア 7ポイント</p>
+      <p>No3. コスタリカ 5ポイント</p>
+    </div>
+  </el-card>
+
   <h2>最近購入した焙煎豆</h2>
   <roasted-bean-list />
   <router-link to="/users/me/roasted_beans">すべて見る</router-link>
