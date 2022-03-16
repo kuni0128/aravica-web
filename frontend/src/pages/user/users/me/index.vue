44ceeac8f35110f6ba056d53af6dd3695bb6cb3f
kuniaki <k.ishikawa.0128@gmail.com>
2022-03-17 07:51:17 +0900

    fix: user profile element

--- frontend/src/pages/user/users/me/index.vue
+++ frontend/src/pages/user/users/me/index.vue
@@ -2,10 +2,16 @@
   <div>
     <el-card>
       <div>
-        <div>K.Iさんのコーヒー生産国ランキング</div>
-        <p>No1. ケニア 10ポイント</p>
-        <p>No2. エチオピア 7ポイント</p>
-        <p>No3. コスタリカ 5ポイント</p>
+        <div class="profile">
+          <img
+            src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+            class="user-image"
+          />
+          <span class="user-name">Kuniaki Ishikawa</span>
+          <span class="in-a-word">
+            コーヒー好きが高じてこのAravicaというサービスを作りました。
+          </span>
+        </div>
       </div>
     </el-card>
 
@@ -33,6 +39,27 @@ import BrewingCoffeeList from "~/components/brewing-coffee-list.vue"
 </script>
 
 <style lang="scss" scoped>
+.profile {
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+
+  .user-image {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+  }
+
+  .user-name {
+    margin-top: 4px;
+    font-weight: bold;
+  }
+
+  .in-a-word {
+    margin-top: 4px;
+    font-size: 13px;
+  }
+}
 .card-list {
   display: flex;
   flex-direction: column;
