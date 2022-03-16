6b3e944d7ddf2cd081715e71c0d0b40b7d7fa952
kuniaki <k.ishikawa.0128@gmail.com>
2022-03-17 08:06:48 +0900

    feat: add edit button

--- frontend/src/pages/user/users/me/index.vue
+++ frontend/src/pages/user/users/me/index.vue
@@ -3,10 +3,13 @@
     <el-card>
       <div>
         <div class="profile">
-          <img
-            src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-            class="user-image"
-          />
+          <div class="top-area">
+            <img
+              src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+              class="user-image"
+            />
+            <el-button class="edit-button">編集</el-button>
+          </div>
           <span class="user-name">Kuniaki Ishikawa</span>
           <span class="in-a-word">
             コーヒー好きが高じてこのAravicaというサービスを作りました。
@@ -44,10 +47,20 @@ import BrewingCoffeeList from "~/components/brewing-coffee-list.vue"
   flex-direction: column;
   align-items: start;
 
-  .user-image {
-    width: 80px;
-    height: 80px;
-    border-radius: 50%;
+  .top-area {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+
+    .user-image {
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+    }
+
+    .edit-button {
+      align-self: start;
+    }
   }
 
   .user-name {
