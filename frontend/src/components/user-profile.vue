9224528e51edf13ae89f1a42067993edec2c5a6b
kuniaki <k.ishikawa.0128@gmail.com>
2022-03-18 07:33:29 +0900

    fix: add thumbnail upload form

--- frontend/src/components/user-profile.vue
+++ frontend/src/components/user-profile.vue
@@ -2,10 +2,7 @@
   <el-card>
     <div class="profile">
       <div class="top-area">
-        <img
-          src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-          class="user-image"
-        />
+        <img :src="thumbnail" class="user-image" />
         <el-button @click="editDialogVisible = true" class="edit-button">
           編集
         </el-button>
@@ -15,6 +12,16 @@
           width="90vw"
         >
           <el-form :model="editDialogForm">
+            <el-form-item label="写真" label-width="100px">
+              <el-upload action="" :auto-upload="false">
+                <template #default>
+                  <div class="edit-dialog-thumbnail">
+                    <img :src="editDialogForm.thumbnail" class="user-image" />
+                    <el-button>ファイルを選択</el-button>
+                  </div>
+                </template>
+              </el-upload>
+            </el-form-item>
             <el-form-item label="ユーザー名" label-width="100px">
               <el-input
                 v-model="editDialogForm.userName"
@@ -44,9 +51,7 @@
         </el-dialog>
       </div>
       <span class="user-name">{{ userName }}</span>
-      <span class="introduction">
-        コーヒー好きが高じてこのAravicaというサービスを作りました。
-      </span>
+      <span class="introduction">{{ introduction }}</span>
     </div>
   </el-card>
 </template>
@@ -54,9 +59,19 @@
 <script setup lang="ts">
 import { reactive, ref } from "vue"
 
+const thumbnail = ref(
+  "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+)
 const userName = ref("Kuniaki Ishikawa")
+const introduction = ref(
+  "コーヒー好きが高じてこのAravicaというサービスを作りました。"
+)
 const editDialogVisible = ref(false)
-const editDialogForm = reactive({ userName: "", introduction: "" })
+const editDialogForm = reactive({
+  thumbnail: thumbnail.value,
+  userName: userName.value,
+  introduction: introduction.value,
+})
 </script>
 
 <style lang="scss" scoped>
@@ -89,6 +104,15 @@ const editDialogForm = reactive({ userName: "", introduction: "" })
   .introduction {
     margin-top: 4px;
     font-size: 13px;
+  }
+}
+
+.edit-dialog-thumbnail {
+  display: flex;
+  gap: 14px;
+
+  .el-button {
+    align-self: end;
   }
 }
 </style>
