250797ad66dc3448dd0effc2efac753a7d460d03
kuniaki <k.ishikawa.0128@gmail.com>
2022-03-18 06:55:11 +0900

    fix: add edit dialog

--- frontend/src/components/user-profile.vue
+++ frontend/src/components/user-profile.vue
@@ -6,15 +6,58 @@
           src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
           class="user-image"
         />
-        <el-button class="edit-button">編集</el-button>
+        <el-button @click="editDialogVisible = true" class="edit-button">
+          編集
+        </el-button>
+        <el-dialog
+          v-model="editDialogVisible"
+          title="プロフィールを編集"
+          width="90vw"
+        >
+          <el-form :model="editDialogForm">
+            <el-form-item label="ユーザー名" label-width="100px">
+              <el-input
+                v-model="editDialogForm.userName"
+                maxlength="20"
+                show-word-limit
+              />
+            </el-form-item>
+            <el-form-item label="自己紹介" label-width="100px">
+              <el-input
+                v-model="editDialogForm.introduction"
+                type="textarea"
+                maxlength="60"
+                show-word-limit
+              />
+            </el-form-item>
+          </el-form>
+          <template #footer>
+            <span>
+              <el-button @click="editDialogVisible = false">
+                キャンセル
+              </el-button>
+              <el-button type="primary" @click="editDialogVisible = false">
+                変更
+              </el-button>
+            </span>
+          </template>
+        </el-dialog>
       </div>
-      <span class="user-name">Kuniaki Ishikawa</span>
-      <span class="in-a-word">
+      <span class="user-name">{{ userName }}</span>
+      <span class="introduction">
         コーヒー好きが高じてこのAravicaというサービスを作りました。
       </span>
     </div>
   </el-card>
 </template>
+
+<script setup lang="ts">
+import { reactive, ref } from "vue"
+
+const userName = ref("Kuniaki Ishikawa")
+const editDialogVisible = ref(false)
+const editDialogForm = reactive({ userName: "", introduction: "" })
+</script>
 
 <style lang="scss" scoped>
 .profile {
@@ -43,7 +86,7 @@
     font-weight: bold;
   }
 
-  .in-a-word {
+  .introduction {
     margin-top: 4px;
     font-size: 13px;
   }
