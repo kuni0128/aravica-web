45f1874e7776ca17ffafe3b879a0d3811faf4808
kuniaki <k.ishikawa.0128@gmail.com>
2022-03-19 07:07:16 +0900

    fix: refactor style

--- frontend/src/components/roasted-bean-list.vue
+++ frontend/src/components/roasted-bean-list.vue
@@ -1,59 +1,43 @@
 <template>
-  <div>
-    <el-row :gutter="12">
-      <el-col
-        v-for="(o, i) in 4"
-        :key="o"
-        :span="6"
-        :xs="24"
-        :sm="12"
-        :md="8"
-        :lg="6"
-        :xl="6"
-      >
-        <el-card class="card" :body-style="{ padding: '12px' }">
-          <img
-            src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-            class="image"
-          />
-          <div style="padding: 14px">
-            <span>イルガチェフェ G1</span>
-            <div class="bottom">
-              <time class="time">{{ today }}</time>
-            </div>
-          </div>
-        </el-card>
-      </el-col>
-    </el-row>
-  </div>
+  <el-row :gutter="12">
+    <el-col :span="6" :xs="24" :sm="12" :md="8" :lg="6" :xl="6">
+      <el-card class="card" :body-style="{ padding: '12px' }">
+        <img
+          src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+          class="image"
+        />
+        <div class="content">
+          <div>エチオピア イルガチェフェ G1</div>
+          <div>自家焙煎珈琲 隠房</div>
+          <time class="time">{{ today }}</time>
+        </div>
+      </el-card>
+    </el-col>
+  </el-row>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue"
 
-const today = ref(new Date())
+const today = ref(new Date().toDateString())
 </script>
 
 <style lang="scss">
-.time {
-  font-size: 13px;
-  color: #999;
-}
-
 .card {
   margin-bottom: 12px;
-}
 
-.bottom {
-  margin-top: 13px;
-  line-height: 12px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
+  .image {
+    width: 100%;
+    display: block;
+  }
 
-.image {
-  width: 100%;
-  display: block;
+  .content {
+    padding-top: 8px;
+
+    .time {
+      font-size: 13px;
+      color: #999;
+    }
+  }
 }
 </style>
