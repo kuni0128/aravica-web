c2bc1086ac831995c3cde6cc3c7d6b20d2edb01a
kuniaki <k.ishikawa.0128@gmail.com>
2022-03-19 14:42:15 +0900

    fix: add kind-label

--- frontend/src/components/roasted-bean-list.vue
+++ frontend/src/components/roasted-bean-list.vue
@@ -2,13 +2,14 @@
   <el-row :gutter="12">
     <el-col :span="6" :xs="24" :sm="12" :md="8" :lg="6" :xl="6">
       <el-card class="card" :body-style="{ padding: '12px' }">
+        <div class="kind-label">シングルオリジン</div>
         <img
           src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
           class="image"
         />
         <div class="content">
           <div class="name">エチオピア イルガチェフェ G1</div>
-          <div class="detail">ハイロースト：N</div>
+          <div class="detail">ハイロースト／N</div>
           <div>自家焙煎珈琲 隠房</div>
           <time class="time">2022/3/19</time>
         </div>
@@ -16,13 +17,14 @@
     </el-col>
     <el-col :span="6" :xs="24" :sm="12" :md="8" :lg="6" :xl="6">
       <el-card class="card" :body-style="{ padding: '12px' }">
+        <div class="kind-label">ブレンド</div>
         <img
           src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
           class="image"
         />
         <div class="content">
           <div class="name">隠房ブレンド</div>
-          <div class="detail">シティロースト：W</div>
+          <div class="detail">シティロースト／W</div>
           <div>自家焙煎珈琲 隠房</div>
           <time class="time">2022/3/12</time>
         </div>
@@ -30,13 +32,14 @@
     </el-col>
     <el-col :span="6" :xs="24" :sm="12" :md="8" :lg="6" :xl="6">
       <el-card class="card" :body-style="{ padding: '12px' }">
+        <div class="kind-label">シングルオリジン</div>
         <img
           src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
           class="image"
         />
         <div class="content">
           <div class="name">ケニア</div>
-          <div class="detail">シティロースト：W</div>
+          <div class="detail">シティロースト／W</div>
           <div>自家焙煎珈琲 隠房</div>
           <time class="time">2022/2/26</time>
         </div>
@@ -44,13 +47,14 @@
     </el-col>
     <el-col :span="6" :xs="24" :sm="12" :md="8" :lg="6" :xl="6">
       <el-card class="card" :body-style="{ padding: '12px' }">
+        <div class="kind-label">ブレンド</div>
         <img
           src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
           class="image"
         />
         <div class="content">
           <div class="name">F・ハニー</div>
-          <div class="detail">シティロースト：W&N</div>
+          <div class="detail">シティロースト／W&N</div>
           <div>自家焙煎珈琲 隠房</div>
           <time class="time">2022/2/20</time>
         </div>
@@ -63,7 +67,23 @@
 
 <style lang="scss">
 .card {
-  margin-bottom: 12px;
+  position: relative;
+
+  .kind-label {
+    position: absolute;
+    left: 0;
+    top: 0;
+    box-sizing: border-box;
+    padding: 0 8px;
+    margin: 0;
+    height: 24px;
+    line-height: 24px;
+    font-size: 10px;
+    letter-spacing: 0.1em;
+    color: white;
+    background: var(--secondary-color);
+    box-shadow: 0 2px 2px rgba(0, 0, 0, 0.12);
+  }
 
   .image {
     width: 100%;
