c5a72ac98aef76fb6cf85dc8db67475579d9c2e9
kuniaki <k.ishikawa.0128@gmail.com>
2022-03-19 15:10:15 +0900

    fix: refactor to pass roasted-beans as props

--- frontend/src/pages/user/users/me/index.vue
+++ frontend/src/pages/user/users/me/index.vue
@@ -4,7 +4,7 @@
 
     <div class="card-list">
       <h2>焙煎豆</h2>
-      <roasted-bean-list />
+      <roasted-bean-list :roasted-beans="roastedBeans" />
       <router-link class="more-link" to="/users/me/roasted_beans">
         焙煎豆をもっと見る
       </router-link>
@@ -24,6 +24,41 @@
 import RoastedBeanList from "~/components/roasted-bean-list.vue"
 import BrewingCoffeeList from "~/components/brewing-coffee-list.vue"
 import UserProfile from "~/components/user-profile.vue"
+
+const roastedBeans = [
+  {
+    name: "エチオピア イルガチェフェ G1",
+    kind: "シングルオリジン",
+    roast: "ハイロースト",
+    processing: "N",
+    roastingShopName: "自家焙煎珈琲 隠房",
+    postedOn: "2022/3/19",
+  },
+  {
+    name: "隠房ブレンド",
+    kind: "ブレンド",
+    roast: "シティロースト",
+    processing: "W",
+    roastingShopName: "自家焙煎珈琲 隠房",
+    postedOn: "2022/3/12",
+  },
+  {
+    name: "ケニア",
+    kind: "シングルオリジン",
+    roast: "シティロースト",
+    processing: "W",
+    roastingShopName: "自家焙煎珈琲 隠房",
+    postedOn: "2022/2/26",
+  },
+  {
+    name: "F・ハニー",
+    kind: "ブレンド",
+    roast: "シティロースト",
+    processing: "W&N",
+    roastingShopName: "自家焙煎珈琲 隠房",
+    postedOn: "2022/2/20",
+  },
+]
 </script>
 
 <style lang="scss" scoped>
--- frontend/src/pages/user/users/me/roasted_beans/index.vue
+++ frontend/src/pages/user/users/me/roasted_beans/index.vue
@@ -1,8 +1,67 @@
 <template>
   <h2>焙煎豆</h2>
-  <roasted-bean-list />
+  <roasted-bean-list :roasted-beans="roastedBeans" />
 </template>
 
 <script setup lang="ts">
 import RoastedBeanList from "~/components/roasted-bean-list.vue"
+
+const roastedBeans = [
+  {
+    name: "エチオピア イルガチェフェ G1",
+    kind: "シングルオリジン",
+    roast: "ハイロースト",
+    processing: "N",
+    roastingShopName: "自家焙煎珈琲 隠房",
+    postedOn: "2022/3/19",
+  },
+  {
+    name: "隠房ブレンド",
+    kind: "ブレンド",
+    roast: "シティロースト",
+    processing: "W",
+    roastingShopName: "自家焙煎珈琲 隠房",
+    postedOn: "2022/3/12",
+  },
+  {
+    name: "ケニア",
+    kind: "シングルオリジン",
+    roast: "シティロースト",
+    processing: "W",
+    roastingShopName: "自家焙煎珈琲 隠房",
+    postedOn: "2022/2/26",
+  },
+  {
+    name: "F・ハニー",
+    kind: "ブレンド",
+    roast: "シティロースト",
+    processing: "W&N",
+    roastingShopName: "自家焙煎珈琲 隠房",
+    postedOn: "2022/2/20",
+  },
+  {
+    name: "ONIBUSブレンド",
+    kind: "ブレンド",
+    roast: "ハイロースト",
+    processing: "W",
+    roastingShopName: "ONIBUS COFFEE",
+    postedOn: "2022/2/18",
+  },
+  {
+    name: "パナマ コトワ ダンカン農園",
+    kind: "シングルオリジン",
+    roast: "ハイロースト",
+    processing: "N",
+    roastingShopName: "自家焙煎珈琲 隠房",
+    postedOn: "2022/2/04",
+  },
+  {
+    name: "彦星ブレンド",
+    kind: "ブレンド",
+    roast: "シティロースト",
+    processing: "W",
+    roastingShopName: "金澤屋珈琲",
+    postedOn: "2022/2/01",
+  },
+]
 </script>
