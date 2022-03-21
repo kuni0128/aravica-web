98312cf7cbbbd3fc746a7b14e72140f190bd08f3
kuniaki <k.ishikawa.0128@gmail.com>
2022-03-21 16:19:35 +0900

    feat: add how to use

--- frontend/src/pages/user/index.vue
+++ frontend/src/pages/user/index.vue
@@ -1,13 +1,35 @@
 <template>
   <el-row>
     <el-col class="story" :span="12" :xs="24">
-      <div class="text-main">美味しいコーヒーをもっと身近に</div>
+      <div class="text-main">好きなコーヒーをもっと知る・楽しむ</div>
       <p>
-        Aravicaは、普段飲んでいるお気に入りのコーヒーを共有することで、他ユーザーや焙煎士と一緒にコーヒーをもっと身近に楽しめるようになるサービスです。
+        Aravicaは、普段飲んでいるお気に入りのコーヒーを共有することで、他のコーヒーLoverや焙煎士と一緒にコーヒーをもっと身近により深く楽しむためのサービスです。
       </p>
     </el-col>
     <el-col :span="12" :xs="24">
       <img class="image" src="src/assets/about.svg" />
+    </el-col>
+  </el-row>
+
+  <el-row class="how-to-use" :gutter="8">
+    <el-col class="title" :span="24">楽しみ方の例</el-col>
+    <el-col :span="8" :xs="24">
+      <el-card shadow="never">
+        <template #header>コーヒーを投稿する</template>
+        お気に入りの焙煎豆の情報や感想を投稿することで、他のユーザーさんが新しいコーヒーと出会うきっかけになります。
+      </el-card>
+    </el-col>
+    <el-col :span="8" :xs="24">
+      <el-card shadow="never">
+        <template #header>色々なコーヒーを知る</template>
+        今まで知らなかったコーヒーとの出会いはいつも新鮮。
+      </el-card>
+    </el-col>
+    <el-col :span="8" :xs="24">
+      <el-card shadow="never">
+        <template #header>焙煎士を応援する</template>
+        丁寧に焙煎してくれている焙煎士に応援や感謝の気持ちを伝えましょう。
+      </el-card>
     </el-col>
   </el-row>
 </template>
@@ -26,6 +48,37 @@
 
   .image {
     width: 30vw;
+  }
+}
+
+.how-to-use {
+  margin-top: 120px;
+
+  .title {
+    display: flex;
+    justify-content: center;
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 16px;
+  }
+
+  .el-card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: #fff9f5;
+    min-height: 180px;
+    margin-bottom: 12px;
+    border: none;
+
+    :deep(.el-card__header) {
+      font-size: 20px;
+      font-weight: bold;
+    }
+
+    :deep(.el-card__body) {
+      font-size: 14px;
+    }
   }
 }
 </style>
